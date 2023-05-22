1cf89198521a114dbee90e0c2055bf798e325102
Jeff-411 <42661647+Jeff-411@users.noreply.github.com>
2023-05-22 10:13:21 -0700

    Change 'select' input to return text not numbers (#7)

--- script/tests/testForm.js
+++ script/tests/testForm.js
@@ -6,10 +6,10 @@ export const test_styleMessages = () => {
   let messages = document.querySelectorAll('.message')
   messages.forEach((message) => {
     message.style.fontSize = `${user.fonts.fontScale}rem`
-
-    if (user.fonts.fontWeight === 1) message.style.fontWeight = 400
-    if (user.fonts.fontWeight === 2) message.style.fontWeight = 600
-    if (user.fonts.fontWeight === 3) message.style.fontWeight = 900
+    message.style.fontWeight = user.fonts.fontWeight
+    // if (user.fonts.fontWeight === 1) message.style.fontWeight = 400
+    // if (user.fonts.fontWeight === 2) message.style.fontWeight = 600
+    // if (user.fonts.fontWeight === 3) message.style.fontWeight = 900
 
     message.style.color = user.fonts.textColors.normal
   })
