ee020c908dcd915223840cc37ff5c6374df491c6
Jeff-411 <42661647+Jeff-411@users.noreply.github.com>
2023-05-27 13:49:14 -0700

    Use consistent <label>/<input> formatting (#13)
    * Refactor 'Templates' folder
    
    * Use consistent <label>/<input> formatting

--- script/formControls/addStandard.js
+++ script/formControls/addStandard.js
@@ -2,7 +2,8 @@ import { config } from '../variables/config.js'
 
 export const addControl_standard = (key, value, control) => {
   // Set the control's label.
-  control.querySelector('label').prepend(config.labelsAndLegends(key))
+  control.querySelector('label').for = key
+  control.querySelector('label').innerText = config.labelsAndLegends(key)
 
   // Set the control's input
   const input = control.querySelector('input')
