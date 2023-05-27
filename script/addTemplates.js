04de82cb1885ebba563f6258083ef5904d884eaf
jeff <johnjeff55@gmail.com>
2023-05-27 07:05:10 -0700

    Refactor 'Templates' folder

--- script/addTemplates.js
+++ script/addTemplates.js
@@ -1,22 +1,17 @@
 export const addTemplates = () => {
   // List of template files to add to the DOM
-  const templateFileNames = [
-    'formControl_color.html',
-    'formControl_fontWeight.html',
-    'formControl_layout.html',
-    'formControl_number.html',
-    'formControl_text.html',
-    'formControlTip_ctrlTriggerBg.html',
-    'formControlTip_fonts.html',
+  const templateFileNames1 = [
+    'simple.html',
+    'special.html',
+    'tips.html',
     'formGroup.html',
   ]
 
   const newDiv = document.createElement('div')
   newDiv.id = 'templates'
-  newDiv.classList.add('templates')
 
   // Add all the files in the templateFileNames array to the newDiv
-  templateFileNames.forEach((fileName) => {
+  templateFileNames1.forEach((fileName) => {
     const path = `./templates/${fileName}`
     fetch(path)
       .then((response) => response.text())
